96dd43f4d7e3d5f0041a04316e21f6f595255933
SandroCJ210 <sandro.carrillo.j@uni.pe>
2025-06-03 19:48:37 -0500

    feat: add validation of jsons for main and network

--- generate_envs.py
+++ generate_envs.py
@@ -1,9 +1,9 @@
 import os, json
 from shutil import copyfile
-
+from jsonschema import validate, ValidationError
 api_key = os.environ.get("API_KEY")
-if api_key is None:
-    raise Exception("No se obtuvo la variable de entorno API_KEY")
+#if api_key is None:
+#    raise Exception("No se obtuvo la variable de entorno API_KEY")
 
 # Parámetros de ejemplo para N entornos
 ENVS = []
@@ -17,6 +17,12 @@ for i in range(1, 11):
 MODULE_DIR = "modules/simulated_app"
 OUT_DIR    = "environments"
 
+with open("modules/simulated_app/network_schema.json") as f:
+    network_schema = json.load(f)
+
+with open("modules/simulated_app/main_schema.json") as f:
+    main_schema = json.load(f)
+
 def render_and_write(env):
     env_dir = os.path.join(OUT_DIR, env["name"])
     os.makedirs(env_dir, exist_ok=True)
@@ -27,10 +33,20 @@ def render_and_write(env):
             os.remove(fpath)
             
     # 1) Copia la definición de variables (network.tf.json)
+    network_dst = os.path.join(env_dir, "network.tf.json")
     copyfile(
         os.path.join(MODULE_DIR, "network.tf.json"),
         os.path.join(env_dir, "network.tf.json")
     )
+
+    with open(network_dst) as f:
+        network_data = json.load(f)
+    try:
+        validate(instance=network_data, schema=network_schema)
+        print(f"network.tf.json válido para {env['name']}")
+    except ValidationError as e:
+        print(f"Error en network.tf.json para {env['name']}: {e.message}")
+        exit(1)
 
     # 2) Genera main.tf.json SÓLO con recursos
     config = {
@@ -63,6 +79,13 @@ def render_and_write(env):
             }
         ]
     }
+
+    try:
+        validate(instance=config, schema=main_schema)
+        print(f"main.tf.json válido para {env['name']}")
+    except ValidationError as e:
+        print(f"Error en main.tf.json para {env['name']}: {e.message}")
+        exit(1)
 
     with open(os.path.join(env_dir, "main.tf.json"), "w") as fp:
         json.dump(config, fp, sort_keys=True, indent=4)
